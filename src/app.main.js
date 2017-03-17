7a9ca8b56dad193fa0082c70bd4600690dad4d24
ppadalka <ppadalka@inbox.ru>
2017-03-17 22:20:24 +0200

    added dust and configured via require js

--- src/app.main.js
+++ src/app.main.js
@@ -3,12 +3,16 @@ requirejs.config({
         'jquery': 'lib/jquery/dist/jquery.min',
         'lodash': 'lib/lodash/dist/lodash.min',
         'underscore': 'lib/lodash/dist/lodash.min',
-        'backbone': 'lib/backbone/backbone-min'
+        'backbone': 'lib/backbone/backbone-min',
+        'dust': 'lib/dustjs-linkedin/dist/dust-full.min'
     },
 
     shim: {
         backbone: {
             deps: ['underscore', 'jquery']
+        },
+        dust: {
+            exports: 'dust'
         }
     }
 });
