d7a089bef749acf50ff5ca0912f816c2ae3c1f39
ppadalka <ppadalka@inbox.ru>
2017-03-17 21:49:12 +0200

    configured build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 const gulp = require('gulp');
 
+gulp.task('copy:index', function () {
+    return gulp.src('src/index.html').pipe(gulp.dest('assets'));
+});
+
 gulp.task('copy:scripts', function () {
-    return gulp.src('src/scripts/**/*.js').pipe(gulp.dest('assets'));
+    return gulp.src('src/scripts/**/*.js')
+        .pipe(gulp.dest('assets/scripts'));
 });
 
 gulp.task('copy:templates', function () {
-    return gulp.src('src/templates/**/*.dust');
+    return gulp.src('src/templates/**/*.dust')
+        .pipe(gulp.dest('assets/templates'));
 });
 
-gulp.task('default', ['copy:scripts', 'copy:templates']);
+gulp.task('copy:require', function () {
+    return gulp.src('src/app.main.js').pipe(gulp.dest('assets'));
+});
+
+gulp.task('default', ['copy:index', 'copy:scripts', 'copy:templates', 'copy:require']);
