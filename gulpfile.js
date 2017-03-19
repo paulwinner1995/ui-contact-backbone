3127f284bf36ba474e5d1123237a6cc881082b4d
ppadalka <ppadalka@inbox.ru>
2017-03-19 23:45:16 +0200

    added template for contact and demo for it

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const less = require('gulp-less');
 
 gulp.task('copy:index', function () {
     return gulp.src('src/index.html').pipe(gulp.dest('assets'));
@@ -14,8 +15,26 @@ gulp.task('copy:templates', function () {
         .pipe(gulp.dest('assets/templates'));
 });
 
+gulp.task('process:less', function () {
+   return gulp.src('src/styles/**/*.less')
+       .pipe(less())
+       .pipe(gulp.dest('assets/styles'))
+});
+
+gulp.task('copy:images', function () {
+    return gulp.src('src/img/**/*.png')
+        .pipe(gulp.dest('assets/img'));
+});
+
 gulp.task('copy:require', function () {
     return gulp.src('src/app.main.js').pipe(gulp.dest('assets'));
 });
 
-gulp.task('default', ['copy:index', 'copy:scripts', 'copy:templates', 'copy:require']);
+gulp.task('default', [
+    'copy:index',
+    'copy:scripts',
+    'copy:templates',
+    'copy:require',
+    'copy:images',
+    'process:less'
+]);
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,15 @@
-define(function (require) {});
+define(function (require) {
+    var ContactView = require('scripts/view/contact.view');
+    var Backbone = require('backbone');
+    var $ = require('jquery');
+
+    var Contact = Backbone.Model.extend({});
+    var testContact = new Contact({
+        name: 'Paul Winner',
+        phone: '0-95-86-99-152',
+        group: 'Family',
+        sex: 'male'
+    });
+
+    new ContactView({el: $('#page-content'), model: testContact}).render();
+});
